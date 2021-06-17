89b5d89d9251d7b0ce4b057abe9510a51a75359a
ArtsiomYermakou <artem_ermakov_1999@mail.ru>
2021-06-17 20:59:54 +0300

    Initial Commit

---
+++ Hello world/Hello world/Hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_worldApp.swift
+//  Hello world
+//
+//  Created by Артём Ермаков on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
