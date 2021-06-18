007ad4f738f1629edce4246656481b634ad75c6a
ArtsiomYermakou <artem_ermakov_1999@mail.ru>
2021-06-18 20:47:00 +0300

    finish BHcake

--- BirthdayCake.playground/Contents.swift
+++ BirthdayCake.playground/Contents.swift
@@ -1,43 +1,23 @@
-class BirthdayCake{
-    var birthdayAge: Int
-    var birthdayName: String
-    var feeds: Int?
-    
-    init(_ age:Int,_ name: String,_ feedNUM: Int) {
-        birthdayAge = age
-        birthdayName = name
-        feeds = feedNUM
+class FarmAnimal {
+    var name:String = "Home pets"
+    var numberOfLegs: Int = 4
+    func sayHello() -> String {
+        return "Hello, i am home animal"
     }
-
-    func message(isinclude: Bool) -> String{
-        return isinclude
-            ? "Happy Birthday \(birthdayName)! You have \(birthdayAge)"
-            : "You have \(birthdayAge)"
-    }
-    
-    func rightYear() -> String {
-        var year = "years"
-        let remainder = birthdayAge % 10
-        switch remainder {
-        case 1:
-            if birthdayAge != 11 {
-                year = "year"
-            }
-        case 2,3,4:
-            if birthdayAge !== 12 && birthdayAge !== 13 && birthdayAge !== 14{
-                year = "YEEEEEAR"
-            }
-        default:
-            break
-        }
-        
+    func description() -> () {
+        print("I'am \(name), and i have \(numberOfLegs) legs.")
     }
 }
 
-var newClass = BirthdayCake(12, "FASOL", 2002)
-var newClass12 = BirthdayCake(3214, "MORKOW", 683)
-//print(newClass.birthdayAge, newClass.birthdayName, newClass.feeds!)
-//print(newClass12.birthdayAge, newClass12.birthdayName, newClass12.feeds!)
 
-var cool = BirthdayCake(54, "Yaroslav", 2).message(isinclude: true)
-print(cool)
+class Chicken: FarmAnimal {
+    var numberOfEggs = 0
+    override init() {
+    super.init()
+            name = "Chicken"
+            numberOfLegs = 2
+    }
+    
+}
+
+
